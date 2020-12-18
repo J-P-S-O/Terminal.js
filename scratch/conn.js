c1f51d110023cf05eb1661d3adeab1ef8a18a645
JPSO <66487668+J-P-S-O@users.noreply.github.com>
2020-12-18 08:59:01 -0300

    Delete conn.js

--- scratch/conn.js
+++
@@ -1,9 +0,0 @@
-function connect(port) {
- return new Promise(resolve => {
-  const socket = net.createConnection(port, '127.0.0.1', () => {
-   socket.write('GET http://127.0.0.1:8080/ HTTP', () => {
-    socket.destroy();
-    resolve();
-   });
-  });
- });}
