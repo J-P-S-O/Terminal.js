8ab293f0372742c82eda1a990cdccb70fec649c3
JPSO <jpso.guerreiro@gmail.com>
2021-01-17 17:17:29 +0000

    .

--- bin/cli.ts
+++ bin/cli.ts
@@ -1,3 +1,14 @@
 #! usr/bin/node
 // this file is the one you run through the cli
 import colorize from "./.."
+import * as cli from "./.."
+// @ts-ignore
+if (process.argv[0] == "node"){
+    console.error("This shall be used from bin ")
+    // @ts-ignore
+    process.exit(1)
+}
+//@ts-ignore 
+let color = process.argv[1]
+// @ts-ignore 
+console.log(colorize(process.argv.slice(2).join(""), color))
